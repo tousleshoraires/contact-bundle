3df44b1f6860865b0b6735606e2d3cb831497d5a
tousleshoraires <j.devergnies@tousleshoraires.fr>
2016-08-31 13:38:11 +0200

    Create TLHContactBundle.php

---
+++ TLHContactBundle.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the TLHContactBundle package.
+ *
+ * (c) TousLesHoraires <https://github.com/tousleshoraires/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace TLH\ContactBundle;
+
+/**
+ * @author Julien Devergnies <julien@tousleshoraires.fr>
+ */
+class TLHContactBundle extends Bundle
+{
+
+}
