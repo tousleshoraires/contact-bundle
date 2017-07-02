fe91f237c787ff330d95bdd4b7f9c53a3107fc43
Encre Informatique <encreinformatique@users.noreply.github.com>
2017-07-02 19:51:08 +0200

    Configuration/travis (#4)
    * Modification of TravisFile
    
    We still have some PHPunit testing pending.
    
    * autoload file
    
    We included an autoload file.
    
    * Travis with Symfony version
    
    We pay attention to the version of PHP for Symphony 3.*
    
    * Testing with Travis
    
    * More Testing Travis
    
    Adding Symfony 3.3
    Verify PHP 5.4.*
    
    * Configuration Travis et Testing
    
    The testing has been properly fixed.
    
    The configuration for Travis has been modified.
    
    * Configuration Travis CI
    
    We made other modification. We added the notification email and the
    phpunit script.
    
    Symfony 2.8.x only on PHP 5.x
    
    * Added phpunit
    
    * Travis
    
    * Travis
    
    * Travis
    
    * Change composer, gitignore, autoload
    
    The autoload has been modified.
    The composer.json has been adjusted for the Testing.
    
    * Testing only the bundle.
    
    * Symfony 3.1 will not be supporting in July 2017
    
    Symfony 3.1 will not be supporting in July 2017. It will be removed in a
    next version.
    
    * Travis minus Symfony 3.1

--- Form/ContactType.php
+++ Form/ContactType.php
@@ -19,6 +19,23 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ContactType extends AbstractType
 {
+    /**
+     * @var string
+     */
+    private $class;
+
+    /**
+     * @param string $class
+     */
+    public function __construct($class = null)
+    {
+        if (is_null($class)) {
+            $this->class = '\TLH\ContactBundle\Entity\Contact';
+        } else {
+            $this->class = $class;
+        }
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('email', EmailType::class, array());
@@ -28,7 +45,7 @@ class ContactType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => null,
+            'data_class' => $this->class,
         ));
     }
 
---
+++ Tests/Form/ContactTypeTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace TLH\ContactBundle\Tests\Form;
+
+use Symfony\Component\Form\Test\TypeTestCase;
+use TLH\ContactBundle\Form\ContactType;
+use TLH\ContactBundle\Entity\Contact;
+
+class ContactTypeTest extends TypeTestCase
+{
+    /**
+     * @test
+     */
+    public function formIsSubmitedSuccessfully()
+    {
+        $contact = new Contact();
+
+        $form = $this->factory->create(ContactType::class, $contact);
+
+        $formData = array(
+            'email' => 'bar@domain.tld',
+            'message' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        );
+        $form->submit($formData);
+
+        $this->assertTrue($form->isSynchronized());
+        $this->assertSame($contact, $form->getData());
+        $this->assertSame('bar@domain.tld', $contact->getEmail());
+    }
+
+    /**
+     * @return array
+     */
+    protected function getTypes()
+    {
+        return array_merge(parent::getTypes(), array(
+            new ContactType('TLH\ContactBundle\Entity\Contact'),
+        ));
+    }
+}
